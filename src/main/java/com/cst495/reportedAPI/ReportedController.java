7b4405d17608285aa0e62995540cfa1a371cfea3
unknown <dochoa-aguila@csumb.edu>
2019-04-08 15:40:27 -0700

    logger set up

--- src/main/java/com/cst495/reportedAPI/ReportedController.java
+++ src/main/java/com/cst495/reportedAPI/ReportedController.java
@@ -2,8 +2,13 @@ package com.cst495.reportedAPI;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 @RestController
 public class ReportedController {
+
+    Logger logger = Logger.getLogger(ReportedController.class.getName());
 
     @CrossOrigin(origins = "*")
     @GetMapping("test")
@@ -14,7 +19,7 @@ public class ReportedController {
     @CrossOrigin(origins = "*")
     @PostMapping("testpost")
     public Response testPostCall(@RequestBody String name){
-        System.out.println(name);
+        logger.log(Level.ALL, name);
         return new Response("Done with Post Call", false);
     }
 }
