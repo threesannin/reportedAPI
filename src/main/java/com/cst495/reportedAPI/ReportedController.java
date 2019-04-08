c21b02038ba4b88446e89c6ebe381160782ae829
unknown <dochoa-aguila@csumb.edu>
2019-04-08 15:12:28 -0700

    testing post mehod

--- src/main/java/com/cst495/reportedAPI/ReportedController.java
+++ src/main/java/com/cst495/reportedAPI/ReportedController.java
@@ -1,8 +1,6 @@
 package com.cst495.reportedAPI;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ReportedController {
@@ -11,5 +9,12 @@ public class ReportedController {
     @GetMapping("test")
     public Response testCall(){
         return new Response("test result", false);
+    }
+
+    @CrossOrigin(origins = "*")
+    @PostMapping("testpost")
+    public Response testPostCall(@RequestBody String name){
+        System.out.println(name);
+        return new Response("Done with Post Call", false);
     }
 }
