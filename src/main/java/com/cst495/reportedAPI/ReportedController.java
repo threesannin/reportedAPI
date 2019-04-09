06dabca8de2846acc04f7e1caaf5dc238c839226
unknown <dochoa-aguila@csumb.edu>
2019-04-08 17:47:57 -0700

    application form urlencoded test

--- src/main/java/com/cst495/reportedAPI/ReportedController.java
+++ src/main/java/com/cst495/reportedAPI/ReportedController.java
@@ -1,7 +1,6 @@
 package com.cst495.reportedAPI;
 
 import org.springframework.http.MediaType;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.logging.Level;
@@ -30,7 +29,7 @@ public class ReportedController {
             consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     //, produces = {MediaType.APPLICATION_ATOM_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
     public @ResponseBody Response authenticate(Response paramMap){
-        if(paramMap == null || paramMap.getStatus().isEmpty()) {
+        if(paramMap == null || paramMap.getStatus().equals("")) {
             return new Response("null error", true);
         }
         return new Response(paramMap.getStatus(), false);
